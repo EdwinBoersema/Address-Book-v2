3d15a64f4b19d1d692a16c95ef42d7d367c0756e
EdwinBoersema <edwinboersema@gmail.com>
2019-10-25 15:26:33 +0200

    Added display functionality to index.ejs

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,9 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     const rows = document.querySelectorAll("tr[data-href]");
+    const pages = document.getElementsByClassName("resultsPage");
+    const buttons = document.getElementsByClassName("pageButton");
 
     rows.forEach(row => {
         row.addEventListener("click", () => {
             window.location.href = row.dataset.href;
         });
     });
+
+
+    for(let i = 0; i < pages.length; i++) {
+        pages[i].style.display = "none";
+    }
+    pages[0].style.display = "block";
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", () => {
+            for(let i = 0; i < pages.length; i++) {
+                pages[i].style.display = "none";
+            }
+            pages[i].style.display = "block";
+        });
+    }
+
 });
